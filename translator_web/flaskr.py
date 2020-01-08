a78cbb45df1eb79e477a13a56fcec359e9e95d2e
CHriss <christophervistal25@gmail.com>
2020-01-08 10:41:13 +0800

    Add new language

--- translator_web/flaskr.py
+++ translator_web/flaskr.py
@@ -4,6 +4,8 @@ from flask import Flask, request, send_from_directory, redirect, render_template
 
 from .eng_to_kamayo_word_translator_predict import EngToKamayoTranslator
 from .kamayo_to_eng_word_translator_predict import KamayoToEngTranslator
+from .kamayo_to_ceb_word_translator_predict import KamayoToCebTranslator
+from .ceb_to_kamayo_word_translator_predict import CebToKamayoTranslator
 
 app = Flask(__name__)
 app.config.from_object(__name__)  # load config from this file , flaskr.py
@@ -15,6 +17,8 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 eng_to_kamayo_w = EngToKamayoTranslator()
 kamayo_to_eng_w = KamayoToEngTranslator()
+kamayo_to_ceb_w = KamayoToCebTranslator()
+ceb_to_kamayo_w = CebToKamayoTranslator()
 
 
 @app.route('/')
@@ -38,7 +42,7 @@ def eng_to_fra_word_translator():
             redirect(request.url)
         else:
             sent = request.form['sentence']
-            translated = eng_to_kamayo_w.translate_lang(sent)
+            translated = ceb_to_kamayo_w.translate_lang(sent)
             return render_template('eng_to_fra_word_translator_result.html', sentence=sent,
                                    translated=translated)
     return render_template('eng_to_fra_word_translator.html')
@@ -47,31 +51,53 @@ def eng_to_fra_word_translator():
 @app.route('/translate', methods=['POST', 'GET'])
 def translate_eng():
     if request.method == 'POST':
-        if not request.json or 'sentence' not in request.json or 'level' not in request.json or 'target_lang' not in request.json:
-            abort(400)
+    #     print(request.form['sentence'])
+    #     if not request.json or 'sentence' not in request.json or 'level' not in request.json or 'target_lang' not in request.json:
+    #         abort(400)
+    #     sentence = request.json['sentence']
+    #     level = request.json['level']
+    #     target_lang = request.json['target_lang']
+    # else:
+    #     sentence = request.args.get('sentence')
+    #     level = request.args.get('level')
+    #     target_lang = request.args.get('target_lang')
+        target_lang = request.json['target_lang']
         sentence = request.json['sentence']
         level = request.json['level']
-        target_lang = request.json['target_lang']
-    else:
-        sentence = request.args.get('sentence')
-        level = request.args.get('level')
-        target_lang = request.args.get('target_lang')
-
-    target_text = sentence
+        target_text = sentence
+        print(request)
 
     if level == 'word' and target_lang == 'kamayo':
-        target_text = eng_to_kamayo_w.translate_lang(sentence)
+        # target_text = eng_to_kamayo_w.translate_lang(sentence)
+        cebu_translated = kamayo_to_ceb_w.translate_lang(sentence)
+        english_translated = kamayo_to_eng_w.translate_lang(sentence)
+
+        return jsonify({
+            'sentence': sentence,
+            'first_output' : cebu_translated + '-cebu',
+            'second_output' : english_translated + '-english',
+        })
+
     elif level == 'word' and target_lang == 'english':
-        print(sentence, target_lang)
-        target_text = kamayo_to_eng_w.translate_lang(sentence)
+        # target_text = kamayo_to_eng_w.translate_lang(sentence)
+        kamayo_translated = eng_to_kamayo_w.translate_lang(sentence)
+        cebu_translated = kamayo_to_ceb_w.translate_lang(kamayo_translated)
 
-    return jsonify({
-        'sentence': sentence,
-        'translated': target_text,
-        'target_lang': target_lang,
-        'level': level
-    })
+        return jsonify({
+            'sentence': sentence,
+            'first_output' : cebu_translated + '-cebu',
+            'second_output' : kamayo_translated + '-kamayo',
+        })
 
+    elif level == 'word' and target_lang == 'ceb':
+        kamayo_translated = ceb_to_kamayo_w.translate_lang(sentence)
+        english_translated = kamayo_to_eng_w.translate_lang(kamayo_translated)
+
+        return jsonify({
+            'sentence': sentence,
+            'first_output' : english_translated + '-english',
+            'second_output' : kamayo_translated + '-kamayo',
+        })
 
 @app.errorhandler(404)
 def not_found(error):
